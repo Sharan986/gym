caf034c2d469bda8a6a296b90adfa0daefc0ecb4
Sumit <sumit9229krsingh@gmail.com>
2025-07-11 18:27:11 +0530

    .assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,28 +1,28 @@
-import vid4 from "../assets/GYMVIDEO/band-chest-fly.mp4";
-import vid2 from "../assets/GYMVIDEO/pec-dec-fly.mp4";
-import vid3 from "../assets/GYMVIDEO/incline-chest-press-machine.mp4";
-import vid1 from "../assets/GYMVIDEO/dumbell-chest-press.mp4";
-import vid5 from "../assets/GYMVIDEO/push-ups.mp4";
-import vid6 from "../assets/GYMVIDEO/machine-rowing.mp4";
-import vid7 from "../assets/GYMVIDEO/band-rowing.mp4";
-import vid8 from "../assets/GYMVIDEO/lats-pulldown.mp4";
-import vid9 from "../assets/GYMVIDEO/dumbbell rowing.mp4";
-import vid10 from "../assets/GYMVIDEO/hypertension.mp4";
-import vid11 from "../assets/GYMVIDEO/lateral-raises.mp4";
-import vid12 from "../assets/GYMVIDEO/front-raises.mp4";
-import vid13 from "../assets/GYMVIDEO/shoulder press.mp4";
-import vid14 from "../assets/GYMVIDEO/shrugs.mp4";
-import vid15 from "../assets/GYMVIDEO/glute-kickbacks.mp4";
-import vid16 from "../assets/GYMVIDEO/dumbbell-calf-raises.mp4";
-import vid17 from "../assets/GYMVIDEO/leg-extension.mp4";
-import vid18 from "../assets/GYMVIDEO/dumbbell-squats.mp4";
-import vid19 from "../assets/GYMVIDEO/45°-leg-press.mp4";
-import vid20 from "../assets/GYMVIDEO/bicep-curl.mp4";
-import vid21 from "../assets/GYMVIDEO/incline-bicep-curl.mp4";
-import vid22 from "../assets/GYMVIDEO/tricep-pushdown.mp4";
-import vid23 from "../assets/GYMVIDEO/dumbbell-overhead-tricep-extensiom.mp4";
-import vid24 from "../assets/GYMVIDEO/hammer-curl.mp4";
-import vid25 from "../assets/GYMVIDEO/chin-ups.mp4";
+import vid4 from "./GYMVIDEO/band-chest-fly.mp4";
+import vid2 from "./GYMVIDEO/pec-dec-fly.mp4";
+import vid3 from "./GYMVIDEO/incline-chest-press-machine.mp4";
+import vid1 from "./GYMVIDEO/dumbell-chest-press.mp4";
+import vid5 from "./GYMVIDEO/push-ups.mp4";
+import vid6 from "./GYMVIDEO/machine-rowing.mp4";
+import vid7 from "./GYMVIDEO/band-rowing.mp4";
+import vid8 from "./GYMVIDEO/lats-pulldown.mp4";
+import vid9 from "./GYMVIDEO/dumbbell rowing.mp4";
+import vid10 from "./GYMVIDEO/hypertension.mp4";
+import vid11 from "./GYMVIDEO/lateral-raises.mp4";
+import vid12 from "./GYMVIDEO/front-raises.mp4";
+import vid13 from "./GYMVIDEO/shoulder press.mp4";
+import vid14 from "./GYMVIDEO/shrugs.mp4";
+import vid15 from "./GYMVIDEO/glute-kickbacks.mp4";
+import vid16 from "./GYMVIDEO/dumbbell-calf-raises.mp4";
+import vid17 from "./GYMVIDEO/leg-extension.mp4";
+import vid18 from "./GYMVIDEO/dumbbell-squats.mp4";
+import vid19 from "./GYMVIDEO/45°-leg-press.mp4";
+import vid20 from "./GYMVIDEO/bicep-curl.mp4";
+import vid21 from "./GYMVIDEO/incline-bicep-curl.mp4";
+import vid22 from "./GYMVIDEO/tricep-pushdown.mp4";
+import vid23 from "./GYMVIDEO/dumbbell-overhead-tricep-extensiom.mp4";
+import vid24 from "./GYMVIDEO/hammer-curl.mp4";
+import vid25 from "./GYMVIDEO/chin-ups.mp4";
 
 
 export const Videos = {
@@ -54,9 +54,9 @@ export const Videos = {
 };
 
 
-import inclinedumbell from "../assets/GYMVIDEO/INTERMEDIATE/CHEST/incline-dumbbell-fly.mp4";
-import pecfly from "../assets/GYMVIDEO/INTERMEDIATE/CHEST/pec-fly.mp4";
-import inclinepress from "../assets/GYMVIDEO/INTERMEDIATE/CHEST/incline-dumbbell-fly.mp4";
+import inclinedumbell from "./GYMVIDEO/INTERMEDIATE/CHEST/incline-dumbbell-fly.mp4";
+import pecfly from "./GYMVIDEO/INTERMEDIATE/CHEST/pec-fly.mp4";
+import inclinepress from "./GYMVIDEO/INTERMEDIATE/CHEST/incline-dumbbell-fly.mp4";
 
 export const interCHEST = {
   inclinepress,
@@ -64,10 +64,10 @@ export const interCHEST = {
   pecfly
 };
 
-import calf_raises from "../assets/GYMVIDEO/INTERMEDIATE/LEGS/calf-raise.mp4";
-import leg_extension from "../assets/GYMVIDEO/INTERMEDIATE/LEGS/leg-extension.mp4";
-import squats from "../assets/GYMVIDEO/INTERMEDIATE/LEGS/squat.mp4";
-import leg_press from "../assets/GYMVIDEO/INTERMEDIATE/LEGS/LEG-PRESS.mp4";
+import calf_raises from "./GYMVIDEO/INTERMEDIATE/LEGS/calf-raise.mp4";
+import leg_extension from "./GYMVIDEO/INTERMEDIATE/LEGS/leg-extension.mp4";
+import squats from "./GYMVIDEO/INTERMEDIATE/LEGS/squat.mp4";
+import leg_press from ".//GYMVIDEO/INTERMEDIATE/LEGS/LEG-PRESS.mp4";
 import RDL from "./GYMVIDEO/INTERMEDIATE/LEGS/Romanian-deadlift-RDL.mp4";
 
 export const interLEGS = {
@@ -78,11 +78,11 @@ export const interLEGS = {
   RDL
 };
 
-import CABLE from "../assets/GYMVIDEO/INTERMEDIATE/SHOULDERS/CABLE-UPRIGHT-ROW.mp4";
-import lateral from "../assets/GYMVIDEO/INTERMEDIATE/SHOULDERS/lateral-raises.mp4";
-import Military from "../assets/GYMVIDEO/INTERMEDIATE/SHOULDERS/military-press.mp4";
-import reverse from "../assets/GYMVIDEO/INTERMEDIATE/SHOULDERS/reverse-fly.mp4";
-import ROPE from "../assets/GYMVIDEO/INTERMEDIATE/SHOULDERS/rope-face-pull.mp4";
+import CABLE from "./GYMVIDEO/INTERMEDIATE/SHOULDERS/CABLE-UPRIGHT-ROW.mp4";
+import lateral from "./GYMVIDEO/INTERMEDIATE/SHOULDERS/lateral-raises.mp4";
+import Military from "./GYMVIDEO/INTERMEDIATE/SHOULDERS/military-press.mp4";
+import reverse from "./GYMVIDEO/INTERMEDIATE/SHOULDERS/reverse-fly.mp4";
+import ROPE from "./GYMVIDEO/INTERMEDIATE/SHOULDERS/rope-face-pull.mp4";
 
 export const interSHOULDERS = {
   CABLE,
@@ -92,12 +92,12 @@ export const interSHOULDERS = {
   ROPE
 };
 
-import bentover from "../assets/GYMVIDEO/INTERMEDIATE/BACK/bent-over-rowing.mp4";
-import chestsupported from "../assets/GYMVIDEO/INTERMEDIATE/BACK/chest-supported-rowing.mp4";
-import closegrid from "../assets/GYMVIDEO/INTERMEDIATE/BACK/close-grip-lat-pulldown.mp4";
-import HYPER from "../assets/GYMVIDEO/INTERMEDIATE/BACK/HYPEREXTENSION.mp4";
-import pullup from "../assets/GYMVIDEO/INTERMEDIATE/BACK/pull-ups.mp4";
-import widegrid from "../assets/GYMVIDEO/INTERMEDIATE/BACK/wide-grip-seated-rowing.mp4";
+import bentover from "./GYMVIDEO/INTERMEDIATE/BACK/bent-over-rowing.mp4";
+import chestsupported from "./GYMVIDEO/INTERMEDIATE/BACK/chest-supported-rowing.mp4";
+import closegrid from "./GYMVIDEO/INTERMEDIATE/BACK/close-grip-lat-pulldown.mp4";
+import HYPER from "./GYMVIDEO/INTERMEDIATE/BACK/HYPEREXTENSION.mp4";
+import pullup from "./GYMVIDEO/INTERMEDIATE/BACK/pull-ups.mp4";
+import widegrid from "./GYMVIDEO/INTERMEDIATE/BACK/wide-grip-seated-rowing.mp4";
 
 export const interBACK = {
   bentover,
@@ -109,11 +109,11 @@ export const interBACK = {
 };
 
 
-import inclinedumbellbicep from "../assets/GYMVIDEO/INTERMEDIATE/ARMS/incline-dumbbell-press.mp4";
-import preacher from "../assets/GYMVIDEO/INTERMEDIATE/ARMS/preacher-curl.mp4";
-import reversecurl from "../assets/GYMVIDEO/INTERMEDIATE/ARMS/reverse-curl.mp4";
-import rope from "../assets/GYMVIDEO/INTERMEDIATE/ARMS/rope-pushdown.mp4";
-import tricep from "../assets/GYMVIDEO/INTERMEDIATE/ARMS/tricep-overhead-extension.mp4";
+import inclinedumbellbicep from "./GYMVIDEO/INTERMEDIATE/ARMS/incline-dumbbell-press.mp4";
+import preacher from "./GYMVIDEO/INTERMEDIATE/ARMS/preacher-curl.mp4";
+import reversecurl from "./GYMVIDEO/INTERMEDIATE/ARMS/reverse-curl.mp4";
+import rope from "./GYMVIDEO/INTERMEDIATE/ARMS/rope-pushdown.mp4";
+import tricep from "./GYMVIDEO/INTERMEDIATE/ARMS/tricep-overhead-extension.mp4";
 export const interARMS = {
   inclinedumbellbicep,
   preacher,
@@ -122,11 +122,11 @@ export const interARMS = {
   tricep
 };
 
-import cablelateral from "../assets/GYMVIDEO/ADVANCE/ARMS/cable-lateral-raise.mp4";
-import concentration from "../assets/GYMVIDEO/ADVANCE/ARMS/concentration-curl.mp4";
-import inclinedb from "../assets/GYMVIDEO/ADVANCE/ARMS/incline-db-curl.mp4";
-import reversecurls from "../assets/GYMVIDEO/ADVANCE/ARMS/reverse-curls.mp4";
-import tricepkick from "../assets/GYMVIDEO/ADVANCE/ARMS/tricep-kickback.mp4";
+import cablelateral from "./GYMVIDEO/ADVANCE/ARMS/cable-lateral-raise.mp4";
+import concentration from "./GYMVIDEO/ADVANCE/ARMS/concentration-curl.mp4";
+import inclinedb from "./GYMVIDEO/ADVANCE/ARMS/incline-db-curl.mp4";
+import reversecurls from "./GYMVIDEO/ADVANCE/ARMS/reverse-curls.mp4";
+import tricepkick from "./GYMVIDEO/ADVANCE/ARMS/tricep-kickback.mp4";
 
 export const advARMS = {
   cablelateral,
@@ -136,12 +136,12 @@ export const advARMS = {
   tricepkick
 };
 
-import chestbarbell from "../assets/GYMVIDEO/ADVANCE/BACK/chest-supported-barbell-rowing.mp4";
-import dbrowing from "../assets/GYMVIDEO/ADVANCE/BACK/db-rowing.mp4";
-import DEADLIFT from "../assets/GYMVIDEO/ADVANCE/BACK/DEADLIFT.mp4";
-import UPRIGHTCABLE from "../assets/GYMVIDEO/ADVANCE/BACK/UPRIGHT-CABLE-ROW.mp4";
-import widegrip from "../assets/GYMVIDEO/ADVANCE/BACK/wide-grip-seated-rowing.mp4";
-import widegriplat from "../assets/GYMVIDEO/ADVANCE/BACK/wide-grip-lat-pulldown.mp4";
+import chestbarbell from "./GYMVIDEO/ADVANCE/BACK/chest-supported-barbell-rowing.mp4";
+import dbrowing from "./GYMVIDEO/ADVANCE/BACK/db-rowing.mp4";
+import DEADLIFT from "./GYMVIDEO/ADVANCE/BACK/DEADLIFT.mp4";
+import UPRIGHTCABLE from "./GYMVIDEO/ADVANCE/BACK/UPRIGHT-CABLE-ROW.mp4";
+import widegrip from "./GYMVIDEO/ADVANCE/BACK/wide-grip-seated-rowing.mp4";
+import widegriplat from "./GYMVIDEO/ADVANCE/BACK/wide-grip-lat-pulldown.mp4";
 
 export const advBACK = {
   chestbarbell,
@@ -152,12 +152,12 @@ export const advBACK = {
   widegriplat
 };
 
-import bench_press from "../assets/GYMVIDEO/ADVANCE/CHEST/bench-press.mp4";
-import cablepress from "../assets/GYMVIDEO/ADVANCE/CHEST/cable-decline-chest-press.mp4";
-import inclinedpress from "../assets/GYMVIDEO/ADVANCE/CHEST/incline-db-press.mp4";
-import parallel from "../assets/GYMVIDEO/ADVANCE/CHEST/parallel-bar-dips.mp4";
-import pecdec from "../assets/GYMVIDEO/ADVANCE/CHEST/pec-dec-fly.mp4";
-import pushup from "../assets/GYMVIDEO/ADVANCE/CHEST/push-ups.mp4";
+import bench_press from "./GYMVIDEO/ADVANCE/CHEST/bench-press.mp4";
+import cablepress from "./GYMVIDEO/ADVANCE/CHEST/cable-decline-chest-press.mp4";
+import inclinedpress from "./GYMVIDEO/ADVANCE/CHEST/incline-db-press.mp4";
+import parallel from "./GYMVIDEO/ADVANCE/CHEST/parallel-bar-dips.mp4";
+import pecdec from "./GYMVIDEO/ADVANCE/CHEST/pec-dec-fly.mp4";
+import pushup from "./GYMVIDEO/ADVANCE/CHEST/push-ups.mp4";
 
 export const advCHEST = {
   bench_press,
@@ -169,13 +169,13 @@ export const advCHEST = {
 };
 
 
-import bulgariasplit from "../assets/GYMVIDEO/ADVANCE/LEGS/bulgarian-split-squat.mp4";
-import hack_squat from "../assets/GYMVIDEO/ADVANCE/LEGS/hack-squat.mp4";
-import hip_adduction from "../assets/GYMVIDEO/ADVANCE/LEGS/hip-adductions.mp4";
-import hip_thrust from "../assets/GYMVIDEO/ADVANCE/LEGS/hip-thrust.mp4";
-import leg_ext from "../assets/GYMVIDEO/ADVANCE/LEGS/leg-extension.mp4";
-import legpress from "../assets/GYMVIDEO/ADVANCE/LEGS/leg-press.mp4";
-import weiged_calf from "../assets/GYMVIDEO/ADVANCE/LEGS/weighed-calf-raises.mp4";
+import bulgariasplit from "./GYMVIDEO/ADVANCE/LEGS/bulgarian-split-squat.mp4";
+import hack_squat from "./GYMVIDEO/ADVANCE/LEGS/hack-squat.mp4";
+import hip_adduction from "./GYMVIDEO/ADVANCE/LEGS/hip-adductions.mp4";
+import hip_thrust from "./GYMVIDEO/ADVANCE/LEGS/hip-thrust.mp4";
+import leg_ext from "./GYMVIDEO/ADVANCE/LEGS/leg-extension.mp4";
+import legpress from "./GYMVIDEO/ADVANCE/LEGS/leg-press.mp4";
+import weiged_calf from "./GYMVIDEO/ADVANCE/LEGS/weighed-calf-raises.mp4";
 
 export const advLEGS = {
   bulgariasplit,
